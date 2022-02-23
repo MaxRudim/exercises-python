c2b9833bc9ef8c8ceea5b2831bd42abc9a936816
MaxRudim <max.hermsdorff@gmail.com>
2022-02-23 18:01:21 -0300

    Correçoes exercico 36

--- ex036.py
+++ ex036.py
@@ -1,10 +1,10 @@
-preço = int(input('Digite o valor da casa que pretende adquirir: R$ '))
-salario = int(input('Digite o seu salário: R$ '))
+preço = float(input('Digite o valor da casa que pretende adquirir: R$ '))
+salario = float(input('Digite o seu salário: R$ '))
 anos = int(input('Digite em até quantos anos pretende pagar a casa: R$ '))
 
 parcelas = anos * 12
 mensalidade = preço / parcelas
-limite = salario + (salario * 30 / 100)
+limite = salario * 30 / 100
 
 if mensalidade > limite:
     print('Financiamento negado! O valor da mensalidade \
