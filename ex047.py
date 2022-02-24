ab1fa61e346964443151844b19e62f90e4843484
MaxRudim <max.hermsdorff@gmail.com>
2022-02-24 18:11:55 -0300

    Exercicio 47 - codigo aprimorado para fazer menos repetiçoes desnecessarias

--- ex047.py
+++ ex047.py
@@ -1,6 +1,7 @@
 pares = list()
 
-for n in range(0, 51):
+# for n in range(0, 51):
+for n in range(2, 51, 2):
     if n % 2 == 0:
         pares.append(n)
 print(', '.join([str(elem) for elem in pares]))
