50d6013d2a889b4f6ab8df449adf423b8d306a96
MaxRudim <max.hermsdorff@gmail.com>
2022-02-24 18:30:06 -0300

    Exercicio 51 - correçao no termo final da PA

--- ex051.py
+++ ex051.py
@@ -3,10 +3,10 @@ print('Montador de Progressões Aritméticas')
 
 t1 = int(input('Digite o primeiro número de uma P.A: '))
 r = int(input('Digite um número para ser a razão de uma P.A: '))
-
+tenth = t1 + (10 - 1) * r
 pa = list()
 
-for num in range(t1, r * 11, r):
+for num in range(t1, tenth + r, r):
     pa.append(num)
 
 print(pa)
