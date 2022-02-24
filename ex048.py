aedaae3bec4bf6daf540a27733ed5d85ccdb0eba
MaxRudim <max.hermsdorff@gmail.com>
2022-02-24 18:16:57 -0300

    Exercicio 48 - adiciona nova forma de resoluçao com menos iteraçoes

--- ex048.py
+++ ex048.py
@@ -3,4 +3,11 @@ sum = 0
 for num in range(1, 501):
     if num % 2 != 0 and num % 3 == 0:
         sum += num
+
+# maneira mais simples de fazer, com menos iterações
+# for num in range(1, 501, 2):
+#     if num % 3 == 0:
+#         sum += num
+
+
 print(sum)
