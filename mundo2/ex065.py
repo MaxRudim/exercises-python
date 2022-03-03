e35714cef4d7dc257d2e740449d216c5f00b5d19
MaxRudim <max.hermsdorff@gmail.com>
2022-03-03 15:46:54 -0300

    Exercicio 65 - Maior e menor valores - bug corrigido

--- mundo2/ex065.py
+++ mundo2/ex065.py
@@ -5,12 +5,13 @@ print('Maior e menor valores.')
 print('-=' * 40)
 
 numbers = list()
-go_on = 'S'
 
-while go_on != 'N':
+while True:
     num = int(input('Digite um número inteiro positivo: '))
     numbers.append(num)
     go_on = str(input('Voce deseja continuar? [S/N] ')).strip().upper()
+    if go_on in 'Nn':
+        break
 
 print(f'A média dos números foi: {sum(numbers) / len(numbers)}')
 print(f'O maior número foi: {max(numbers)} e o menor foi: {min(numbers)}')
