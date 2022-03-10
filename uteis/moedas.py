e0580d6d75e3c6943af0e3bba1b4fe2afbe7f050
MaxRudim <max.hermsdorff@gmail.com>
2022-03-10 14:44:24 -0300

    Exercicio 107 - Exercitando modulos em python

---
+++ uteis/moedas.py
@@ -0,0 +1,14 @@
+def metade(p):
+    return p / 2
+
+
+def dobro(p):
+    return p * 2
+
+
+def aumentar(p, percent):
+    return p + (p * percent / 100)
+
+
+def diminuir(p, percent):
+    return p - (p * percent / 100)
