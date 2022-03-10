42d449e19e3dead903608991572c7ce5606c615a
MaxRudim <max.hermsdorff@gmail.com>
2022-03-10 15:23:42 -0300

    Exercicio 110 - Reduzindo ainda mais o seu programa

---
+++ ex110.py
@@ -0,0 +1,6 @@
+from uteis import moedas
+
+
+p = float(input('Digite o preço: R$ '))
+
+moedas.resumo(1000, 10, 20)
--- uteis/moedas.py
+++ uteis/moedas.py
@@ -30,3 +30,23 @@ def diminuir(p, percent, f=False):
         return moedas(p - (p * percent / 100))
     else:
         return p - (p * percent / 100)
+
+
+def resumo(p, aumento=0, diminuição=0):
+    print('-' * 50)
+    print('RESUMO DO VALOR')
+    print('-' * 50)
+
+    print(f'Preço analisado: {moedas(p)}')
+
+    print(f'A metade de {moedas(p)} é {metade(p, True)}')
+
+    print(f'O dobro de {moedas(p)} é {dobro(p, True)}')
+
+    print(f'Aumentando {aumento}% de {moedas(p)}, \
+temos {aumentar(p, aumento, True)}')
+
+    print(f'Diminuindo {diminuição}% de {moedas(p)}, \
+temos {diminuir(p, diminuição, True)}')
+
+    print('-' * 50)
